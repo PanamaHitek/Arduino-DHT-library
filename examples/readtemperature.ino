c331d90b7be48f1adcd2472ad820f0e972c77dd7
Kiara Navarro <sophiekovalevsky@fedoraproject.org>
2016-02-08 18:31:58 -0500

    Add read temperature examples sketch.

---
+++ examples/readtemperature.ino
@@ -0,0 +1,27 @@
+#include "DHT.h"
+
+#define pin 9
+#define model DHT11
+
+// Instance object
+DTH dht(pin,model);
+
+void setup() {
+	Serial.begin(9600);
+}
+
+void loop() {
+	// Wait few seconds
+	delay(2000);
+
+	float T = dht.readTemperature();
+
+	if (isnan(T)) {
+		Serial.println(F("Something goes wrong... =("));
+		return;
+	}
+
+	Serial.print(F("Temperature: "));
+	Serial.print(T);
+	Serial.print(F(" *C" ));
+}
